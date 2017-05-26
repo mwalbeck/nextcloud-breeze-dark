2cfdcade4d9fc410e3fb4728ee876256038f4185
Magnus Walbeck <mw@mwalbeck.org>
2017-05-26 10:10:30 +0200

    Update defaults.php to NC 12

--- defaults.php
+++ defaults.php
@@ -137,6 +137,6 @@ class OC_Theme {
 	}
 
 	public function buildDocLinkToKey($key) {
-		return $this->getDocBaseUrl() . '/server/11/go.php?to=' . $key;
+		return $this->getDocBaseUrl() . '/server/12/go.php?to=' . $key;
 	}
 }
