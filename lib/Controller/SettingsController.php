d95e9c0cf4a2181e3235e723b4e7a1d81534a83d
Magnus Walbeck <mw@mwalbeck.org>
2021-08-10 21:39:53 +0200

    Add composer file to manage some dev dependencies

--- lib/Controller/SettingsController.php
+++ lib/Controller/SettingsController.php
@@ -4,38 +4,37 @@ declare(strict_types=1);
 
 /**
  * Breeze Dark theme for Nextcloud
- * 
+ *
  * @copyright Copyright (C) 2020  Magnus Walbeck <mw@mwalbeck.org>
- * 
+ *
  * @author Magnus Walbeck <mw@mwalbeck.org>
- * 
+ *
  * @license GNU AGPL version 3 or any later version
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  */
 
 namespace OCA\BreezeDark\Controller;
 
 use OCP\AppFramework\Controller;
-use OCP\AppFramework\Http;
-use OCP\AppFramework\Http\DataDisplayResponse;
 use OCP\IConfig;
 use OCP\IRequest;
 use OCP\IUserSession;
 
-class SettingsController extends Controller {
+class SettingsController extends Controller
+{
 
     /** @var string */
     protected $appName;
@@ -52,10 +51,12 @@ class SettingsController extends Controller {
      * @param IUserSession $userSession
      * @param IRequest $request
      */
-    public function __construct(string $appName,
-                                IConfig $config,
-                                IUserSession $userSession,
-                                IRequest $request) {
+    public function __construct(
+        string $appName,
+        IConfig $config,
+        IUserSession $userSession,
+        IRequest $request
+    ) {
         parent::__construct($appName, $request);
         $this->config = $config;
         $this->userId = $userSession->getUser()->getUID();
@@ -63,10 +64,11 @@ class SettingsController extends Controller {
 
     /**
      * @NoAdminRequired
-     * 
+     *
      * Set user theme option
      */
-    public function personal(): void {
+    public function personal(): void
+    {
         if ($this->request->getParam("theme_enabled")) {
             $this->config->setUserValue($this->userId, $this->appName, "theme_enabled", "1");
         } else {
@@ -77,7 +79,8 @@ class SettingsController extends Controller {
     /**
      * Set global theme option
      */
-    public function admin(): void {
+    public function admin(): void
+    {
         if ($this->request->getParam("theme_enabled")) {
             $this->config->setAppValue($this->appName, "theme_enabled", "1");
         } else {
@@ -94,7 +97,8 @@ class SettingsController extends Controller {
     /**
      * Set custom styling option
      */
-    public function customStyling(): void {
+    public function customStyling(): void
+    {
         if ($this->request->getParam("theme_custom_styling")) {
             $this->config->setAppValue($this->appName, "theme_custom_styling", $this->request->getParam("theme_custom_styling"));
             $this->config->setAppValue($this->appName, "theme_cachebuster", time());
